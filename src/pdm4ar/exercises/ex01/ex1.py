d1df05c07a655bcded23f763d4c997014d75ed47
lucamonegaglia <monegaglialuca@gmail.com>
2024-09-19 14:26:42 +0000

    [submit][01] aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa this is so hard

--- src/pdm4ar/exercises/ex01/ex1.py
+++ src/pdm4ar/exercises/ex01/ex1.py
@@ -21,4 +21,9 @@ def compare_lexicographic(a: Tuple[float], b: Tuple[float]) -> ComparisonOutcome
     but more important than the one in position 2
     """
     # todo: implement the comparison
+    for i, _ in enumerate(a):
+        if a[i] < b[i]:
+            return FIRST_PREFERRED
+        if a[i] > b[i]:
+            return SECOND_PREFERRED
     return INDIFFERENT
